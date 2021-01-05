18bd626a891a6d6bdd2148007ef39702463d17db
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-05 20:17:21 +0500

    (feat) add Categories component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Header from "./components/Header";
+import React from "react";
+import { Categories, Header } from "./components";
 
 function App() {
   return (
@@ -7,16 +8,18 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              onClick={(category) => {
+                console.log(category);
+              }}
+              items={[
+                "Мясные",
+                "Вегетарианская",
+                "Гриль",
+                "Острые",
+                "Закрытые",
+              ]}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
---
+++ src/components/Categories.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Categories = ({ items, onClick }) => {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          onClick={() => setActiveCategory(null)}
+          className={activeCategory === null ? "active" : ""}
+        >
+          Все
+        </li>
+        {items.map((name, index) => (
+          <li
+            onClick={() => setActiveCategory(index)}
+            className={activeCategory === index ? "active" : ""}
+            key={`${name}_${index}`}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
