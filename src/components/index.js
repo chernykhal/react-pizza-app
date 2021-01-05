04b4f1c0d6c87d1a84ff77329cc5040644980b75
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-05 18:55:41 +0500

    (feat) add import components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Logo } from "./../assets/img/pizza-logo.svg";
+export { default as Header } from "./Header";
+export { default as Button } from "./Button";
+export { default as Categories } from "./Categories";
