16d686bdec6d3d4c10a3bc392982d504cbe109ef
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-05 20:17:00 +0500

    (feat) edit Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "../assets/img/pizza-logo.svg";
-import Button from "./Button";
+import { Button, Logo } from ".";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
         <div className="header__logo">
-          <img width="38" src={logo} alt="Pizza logo" />
+          <img width="38" src={Logo} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
