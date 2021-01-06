5192e114584b5e18efd02538284d300e422f787e
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-06 19:52:38 +0500

    (feat) add React Router to project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./scss/app.scss";
 
 import App from "./App";
+import Header from "./components/Header";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
