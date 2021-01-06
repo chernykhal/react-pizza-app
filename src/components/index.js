8b38d1580348a7df2c6bbf0e83b5ab8ad2512552
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-06 19:21:09 +0500

    (feat) add SortPopup component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Logo } from "./../assets/img/pizza-logo.svg";
 export { default as Header } from "./Header";
 export { default as Button } from "./Button";
 export { default as Categories } from "./Categories";
+export { default as SortPopup } from "./SortPopup";
