e1f0f8f25fd8e796896b08f0d6766664e650baf3
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-07 20:05:17 +0500

    (feat) add PizzaBlock component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact component={Home} path="/" />
+        <Route exact render={() => <Home items={pizzas} />} path="/" />
         <Route exact component={Cart} path="/cart" />
       </div>
     </div>
