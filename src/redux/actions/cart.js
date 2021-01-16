ccbef6e7f6a5342bbaff7f16539e1a11d7ee4577
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-16 18:35:39 +0500

    (feat) add cart methods

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,10 @@ export const addPizzaToCart = (pizzaObj) => ({
   type: "ADD_PIZZA_CART",
   payload: pizzaObj,
 });
+export const removeCartItem = (id) => ({
+  type: "REMOVE_CART_ITEM",
+  payload: id,
+});
+export const clearCart = () => ({
+  type: "CLEAR_CART",
+});
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -4,28 +4,59 @@ const initialState = {
   totalCount: 0,
 };
 
+const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_PIZZA_CART":
+      const currentPizzaItems = !state.items[action.payload.id]
+        ? [action.payload]
+        : [...state.items[action.payload.id].items, action.payload];
       const newItems = {
         ...state.items,
-        [action.payload.id]: !state.items[action.payload.id]
-          ? [action.payload]
-          : [...state.items[action.payload.id], action.payload],
+        [action.payload.id]: {
+          items: currentPizzaItems,
+          totalPrice: getTotalPrice(currentPizzaItems),
+        },
       };
-
-      const allPizzas = [].concat.apply([], Object.values(newItems));
+      // const totalCount = Object.keys(newItems).reduce(
+      //   (sum, key) => newItems[key].items.length + sum,
+      //   0
+      // );
+      // const totalPrice = Object.keys(newItems).reduce(
+      //   (sum, key) => newItems[key].totalPrice + sum,
+      //   0
+      // );
+      // const totalCount = totalSum(newItems, "items.length");
+      // const totalPrice = totalSum(newItems, "totalPrice");
       return {
         ...state,
         items: newItems,
-        totalCount: allPizzas.length,
-        totalPrice: allPizzas.reduce((sum, obj) => obj.price + sum, 0),
+        totalCount,
+        totalPrice,
       };
-      break;
     case "SET_TOTAL_COUNT":
       return {
         ...state,
         totalCount: action.payload,
+      };
+    case "REMOVE_CART_ITEM":
+      const newCartItems = {
+        ...state.items,
+      };
+      const currentTotalPrice = newCartItems[action.payload].totalPrice;
+      const currentTotalCount = newCartItems[action.payload].items.length;
+      delete newCartItems[action.payload];
+      return {
+        ...state,
+        items: newCartItems,
+        totalPrice: state.totalPrice - currentTotalPrice,
+        totalCount: state.totalCount - currentTotalCount,
+      };
+    case "CLEAR_CART":
+      return {
+        totalPrice: 0,
+        totalCount: 0,
+        items: {},
       };
     default:
       return state;
